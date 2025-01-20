50b54a1fdabaefe40ae528d5b064a871a053fb3a
bencarneiro <bencarneiro@gmail.com>
2025-01-20 12:29:08 -0600

    add mastodon redirect, favicon view

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.views.generic.base import RedirectView
 
 # Create your views here.
 
@@ -6,3 +8,9 @@ from django.shortcuts import render
 
 def resume(request):
     return render(request, "resume.html")
+
+def mastodon_redirect(request):
+    return redirect("https://its.bencarneiro.com/@ben")
+
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app.views import resume
+from django.urls import path, re_path
+from app.views import resume, mastodon_redirect, favicon_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", resume, name="home"),
+    path("@ben", mastodon_redirect, name="mastodon_redirect"),
+    re_path(r'^favicon\.ico$', favicon_view),
 ]
