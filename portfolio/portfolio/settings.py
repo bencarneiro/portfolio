2b8a551e35b29007616f82a7d2849b71b1800b73
bencarneiro <bencarneiro@gmail.com>
2025-01-20 12:09:57 -0600

    edit settings file to lead statics, and reroute to new mastodon

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/home/tonydeals/app/portfolio/portfolio/app/templates/static/",
+    "/root/app/portfolio/portfolio/app/templates/static/"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
