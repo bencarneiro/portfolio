f3efb053695309e7dcf598ffcc8f5bca9e9d7872
bencarneiro <bencarneiro@gmail.com>
2025-01-20 13:44:07 -0600

    add static root

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     "/home/tonydeals/app/portfolio/portfolio/app/templates/static/",
     "/root/app/portfolio/portfolio/app/templates/static/"
 ]
+
+STATIC_ROOT = "/var/www/static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
